c1e15ec2c35a658102ad14c5417b363a85c40fa7
Mike Scott <mikes@dev1.localdomain>
2011-02-05 22:40:29 +0000

    Added more packages

--- puppet/modules/php5/manifests/init.pp
+++ puppet/modules/php5/manifests/init.pp
@@ -2,7 +2,8 @@ class php5 {
     package { [
             "php5-cgi", "php-apc", "php-pear", "php5-cli",
             "php5-dev", "php5-suhosin", "php5-xdebug", 
-            "php5-mysql"]:
+            "php5-mysql", "php5-common", "libonig2",
+            "libqdbm14", "libmysqlclient16", "phpapi-20090626"]:
         ensure => installed,
     }
 }
