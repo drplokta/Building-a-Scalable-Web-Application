975d6d7a2ad57cce9a879339f8cd2dfd6209b79d
Mike Scott <mike@plokta.com>
2011-02-05 22:43:05 +0000

    Removed php5-dev

--- puppet/modules/php5/manifests/init.pp
+++ puppet/modules/php5/manifests/init.pp
@@ -1,7 +1,7 @@
 class php5 {
     package { [
             "php5-cgi", "php-apc", "php-pear", "php5-cli",
-            "php5-dev", "php5-suhosin", "php5-xdebug", 
+            "php5-suhosin", "php5-xdebug", 
             "php5-mysql", "php5-common", "libonig2",
             "libqdbm14", "libmysqlclient16", "phpapi-20090626"]:
         ensure => installed,
