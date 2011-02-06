4015fce257fa4b7678c02c74fb33ccecebe6d1f7
Mike Scott <mike@plokta.com>
2011-02-06 17:19:06 +0000

    Added PHP modules

--- puppet/modules/php5/manifests/init.pp
+++ puppet/modules/php5/manifests/init.pp
@@ -2,6 +2,7 @@ class php5 {
     package { [
             "php5-cgi", "php-apc", "php-pear", "php5-cli",
             "php5-suhosin", "php5-xdebug", "php5-pgsql",
+            "php5-gd", "php5-mcrypt",
 	]:
         ensure => installed,
     }
