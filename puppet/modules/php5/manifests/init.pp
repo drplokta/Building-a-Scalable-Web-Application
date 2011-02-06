bd94a14951e69e018e4ca32f080c2e620eb6d2b3
Mike Scott <mike@plokta.com>
2011-02-06 09:35:26 +0000

    Added PostgreSQL

--- puppet/modules/php5/manifests/init.pp
+++ puppet/modules/php5/manifests/init.pp
@@ -1,7 +1,7 @@
 class php5 {
     package { [
             "php5-cgi", "php-apc", "php-pear", "php5-cli",
-            "php5-suhosin", "php5-xdebug", 
+            "php5-suhosin", "php5-xdebug", "php5-pgsql",
 	]:
         ensure => installed,
     }
---
+++ puppet/modules/postgresql/manifests/init.pp
@@ -0,0 +1,5 @@
+class postgresql {
+    package ["postgresql-8.4", "postgresql-client"]:
+        ensure => installed,
+    }
+}
