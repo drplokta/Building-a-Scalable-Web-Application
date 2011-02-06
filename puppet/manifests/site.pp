a594d57881621719378013ab3a0eb89db449e047
Mike Scott <mike@plokta.com>
2011-02-06 07:51:26 +0000

    More updates for spawn-fcgi and nginx

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,2 +1,4 @@
+$rootdir = "/root/Building-a-Scalable-Web-Application"
+
 import "classes.pp"
 import "nodes.pp"
--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -2,4 +2,17 @@ class nginx {
     package { nginx:
         ensure => installed,
     }
+
+    file { "/etc/nginx/sites-available/default":
+        source => "puppet:///modules/nginx/default",
+        owner => root,
+        group => root,
+    }
+
+    service { nginx:
+        ensure => running,
+        hasrestart => true,
+        hasstatus => true,
+        subscribe => File["/etc/nginx/sites-available/default"],
+    }
 }
