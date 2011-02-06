03c219c3e4158b6e94d18a0a6e0855ae50b910f8
Mike Scott <mike@plokta.com>
2011-02-06 13:50:15 +0000

    More on beanstalkd

--- puppet/modules/beanstalkd/manifests/init.pp
+++ puppet/modules/beanstalkd/manifests/init.pp
@@ -2,4 +2,22 @@ class beanstalkd {
     package { beanstalkd:
         ensure => installed,
     }
+    
+    file { "/etc/default/beanstalkd":
+        source => "puppet:///modules/beanstalkd/beanstalkd",
+        owner => root,
+        source => root,
+    }
+    
+    file { "/etc/ufw/applications.d/beanstalkd":
+        source => "puppet:///modules/ufw/beanstalkd",
+        owner => root,
+        group => root,
+    }
+
+    exec { "allow-http":
+        command => "/usr/sbin/ufw allow beanstalkd",
+        unless => "/usr/sbin/ufw status | grep \"beanstalkd.*ALLOW.*Anywhere\\|Status: inactive\"",
+        require => Exec["enable-firewall"],
+    }
 }
