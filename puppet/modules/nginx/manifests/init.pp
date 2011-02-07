d7081103eb660e0cd479a24c2ed3cfdb82f196c7
Mike Scott <mike@colourdna.com>
2011-02-07 15:20:41 +0000

    Fixed dependency

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -1,10 +1,10 @@
 class nginx {
     package { "nginx":
         ensure  => installed,
+		require => Package["apache2-mpm-prefork", "apache2.2-bin", "apache2.2-common", "apache2-utils", "apache2-mpm-itk"],
     }
     
     package { ["apache2-mpm-prefork", "apache2.2-bin", "apache2.2-common", "apache2-utils", "apache2-mpm-itk"]:
-		alias  => "apache2-all",
         ensure => absent,
     }
 
