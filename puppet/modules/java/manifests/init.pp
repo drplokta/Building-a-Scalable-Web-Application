b865607c88e85ea97bfd807551054b2a8702a63c
Mike Scott <mike@plokta.com>
2011-02-07 07:28:46 +0000

    Added auto-response for Java install

--- puppet/modules/java/manifests/init.pp
+++ puppet/modules/java/manifests/init.pp
@@ -1,6 +1,12 @@
 class java {
     package { sun-java6-jre:
         ensure => installed,
-        require => File["/etc/apt/sources.list"],
+        require => [File["/etc/apt/sources.list"],File["/var/cache/debconf/jre6.seeds"]],
+        responsefile => "/var/cache/debconf/jre6.seeds",
+    }
+    
+    file { "/var/cache/debconf/jre5.seeds":
+        source => "puppet:///modules/java/jre6.seeds",
+        ensure => present,
     }
 }
