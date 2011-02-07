eec0d5f895351e378f23e837b049822d11c3f7bd
Mike Scott <mike@plokta.com>
2011-02-07 06:54:08 +0000

    Fixed typo

--- puppet/modules/cruisecontrol/manifests/init.pp
+++ puppet/modules/cruisecontrol/manifests/init.pp
@@ -1,5 +1,5 @@
 class cruisecontrol {
-    exec { "unzip $rootdir/puppet/modules/cruisecontrol/cruisecontrol-bin-2.8.4.zip":
+    exec { "unzip $rootdir/puppet/modules/cruisecontrol/files/cruisecontrol-bin-2.8.4.zip":
         cwd => "/opt",
         creates => "/opt/cruisecontrol-bin-2.8.4",
         path => ["/usr/bin"],
