fefa67ee7e56502657174168f9d2cfa38e16c198
Mike Scott <mike@plokta.com>
2011-02-26 15:41:51 +0000

    Moved directory

--- puppet/modules/puppet-update/files/puppet-update.sh
+++ puppet/modules/puppet-update/files/puppet-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /root/Building-a-Scalable-Web-Application
 /usr/bin/git pull origin master
-/usr/bin/puppet apply --confdir=/root/Building-a-Scalable-Web-Application/puppet /root/Building-a-Scalable-Web-Application/puppet/manifests/site.pp
+/usr/bin/puppet apply --confdir=/home/mikes/Building-a-Scalable-Web-Application/puppet /home/mikes/Building-a-Scalable-Web-Application/puppet/manifests/site.pp
 
